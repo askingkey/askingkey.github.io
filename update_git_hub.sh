f244f316b740743f39079e09303968fac11c0fe4
askingkey <jszky929@163.com>
2023-12-14 00:08:08 +0800

    message

--- update_git_hub.sh
+++ update_git_hub.sh
@@ -2,6 +2,7 @@
 
 git add  .
 git commit -m "message"
-git remote set-url origin https://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@github.com/askingkey/askingkey.github.io.git
+# git remote set-url origin https://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@github.com/askingkey/askingkey.github.io.git
+git remote set-url origin https://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@github.com/askingkey/askingkey.github.io.git
 git pull origin main
 git push origin main
