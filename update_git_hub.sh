be3fb191d147b9485fc93e07d19a1ffcbf11a318
askingkey <jszky929@163.com>
2023-12-18 16:15:52 +0800

    message

--- update_git_hub.sh
+++ update_git_hub.sh
@@ -4,6 +4,7 @@ git add  .
 git commit -m "message"
 # git remote set-url origin https://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@github.com/askingkey/askingkey.github.io.git
 # git remote set-url origin https://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@github.com/askingkey/askingkey.github.io.git
-git remote set-url origin https://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@github.com/askingkey/askingkey.github.io.git
+# git remote set-url origin https://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@github.com/askingkey/askingkey.github.io.git
+git remote set-url origin https://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@github.com/askingkey/askingkey.github.io.git
 git pull origin main
 git push origin main
